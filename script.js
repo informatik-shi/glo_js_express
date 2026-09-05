3fc15eaeea2597a6a8c148a3f80f47c3d6b75f01
informatik.shi <informatik.shi@gmail.com>
2026-09-05 08:55:39 +0300

    Выполнить задание урока 02

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const title = 'Учебный проект';
-const screens = 3;
+const title = 'Лендинг проекта';
+const screens = 'Простые, Сложные, Интерактивные';
 const screenPrice = 1000;
 const rollback = 10;
-const fullPrice = screens * screenPrice;
+const fullPrice = 50000;
 const adaptive = true;
 
-alert('Скрипт успешно подключён!');
-console.log('Урок 01 выполнен без ошибок.');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов (${screenPrice}) рублей`);
+console.log(`Стоимость разработки сайта (${fullPrice}) рублей`);
+console.log(screens.toLowerCase().split(', '));
+console.log(`Процент отката посреднику за работу: ${fullPrice * (rollback / 100)} рублей`);
